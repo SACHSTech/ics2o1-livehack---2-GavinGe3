adff1273ccadcfe82d1afaa154f8ba837abcf686
Gavin Ge <gavin.ge23@ycdsbk12.ca>
2021-02-23 15:13:05 +0000

    Final Submission.

--- problem1.py
+++ problem1.py
@@ -1,1 +1,37 @@
 
+"""
+-------------------------------------------------------------------------------
+Name:   problem1.py
+Purpose:  Given an input of the number of antennas and eyes, determines the alien lifeform
+ 
+Author: Ge.G
+ 
+Created:  23/02/2021
+------------------------------------------------------------------------------
+"""
+print("***Welcome to the alien life form detector***")
+
+# get eye and antenna info
+antennas = int(input("How many antennas does the lifeform have?: "))
+eyes = int(input("How many eyes does the lifeform have?: "))
+
+# determines and outputs the alien species
+if antennas <= 6 and eyes >= 2:
+  print("\nLife form detected: MaxMartian")
+if antennas >= 3 and eyes <= 4:
+  print("\nLife form detected: AudreyMartian")
+if antennas <= 2 and eyes <= 3:
+  print("\nLife form detected: BrooklynMartian")
+if antennas == 0 and eyes == 2:
+  print("\nLife form detected: MattDamonMartian")
+if antennas > 6 and eyes > 4:
+  print("\nNo life form detected")
+
+
+
+
+
+
+
+
+
--- problem2.py
+++ problem2.py
@@ -1,1 +1,22 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem2.py
+Purpose:  Given 3 inputted sidelengths, determines if the sidelengths form a triangle
+ 
+Author: Ge.G
+ 
+Created:  23/02/2021
+------------------------------------------------------------------------------
+"""
+print("*****Welcome to the Triangle Checker*****")
 
+# gets sidelength info
+sideOne = float(input("Enter the length of the first side: "))
+sideTwo =  float(input("Enter the length of the second side: "))
+sideThree = float(input("Enter the length of the third side: "))
+
+# determine and output if the sidelengths form  a triangle
+if sideOne + sideTwo > sideThree and sideOne + sideThree > sideTwo and sideTwo + sideThree > sideOne:
+  print(f"\nThe figure with sidelengths {sideOne}, {sideTwo} and {sideThree} is a triangle")
+else:
+  print(f"\nThe figure with sidelengths {sideOne}, {sideTwo} and {sideThree} is not a triangle")
